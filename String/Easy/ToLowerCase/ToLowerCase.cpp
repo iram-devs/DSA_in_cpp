29f25ec3049218a1de88caa4ada4bd6298f28d88
iram-devs <iramshamim2@gmail.com>
2026-09-01 23:24:42 +0530

    added To Lower Case

---
+++ String/Easy/ToLowerCase/ToLowerCase.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i = 0;i<s.size();i++)
+        {
+            if(s[i]>='A' && s[i]<='Z')
+            {
+                s[i]=tolower(s[i]);
+            }
+        }
+        return s;
+    }
+};
+int main()
+{
+    string str = "Hello";
+
+    Solution obj;
+    string ans = obj.toLowerCase(str);
+    cout<<ans<<endl;
+    return 0;
+}
