3234ba6d885c69b9801b1b9ddcae39edcfc5ed33
iram-devs <iramshamim2@gmail.com>
2026-09-12 15:27:29 +0530

    added floor in a BST

---
+++ BinarySearchTree/Easy/FloorInABinarySearchTree/FloorInABinarySearchTree.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(): val(0) , left(nullptr),right(nullptr) {}
+    TreeNode(int x) : val(x) , left(nullptr) , right(nullptr){}
+    TreeNode(int x , TreeNode* left , TreeNode* right) : val(x) , left(left) , right(right){}
+};
+
+class Solution{
+    public:
+    int floorInBST(TreeNode* root , int k) //to find floor value of k
+    {
+        int floor = -1;
+        while(root!=NULL)
+        {
+            if(root->val == k)
+            {
+                floor = k;
+                return floor;
+            }
+            if(root->val<k)
+            {
+                floor = root->val;
+                root=root->right;
+            }
+            else{
+                root=root->left;
+            }
+        }
+        return floor;
+    }
+};
+int main()
+{
+    TreeNode* root = new TreeNode(35);
+    root->left = new TreeNode(23);
+    root->right = new TreeNode(45);
+    root->left->left = new TreeNode(12);
+    root->left->right = new TreeNode(28);
+    root->right->left = new TreeNode(40);
+    root->left->right->left = new TreeNode(25);
+    root->right->left->left = new TreeNode(39);
+    root->right->left->right = new TreeNode(42);
+    
+    Solution obj;
+    int ans = obj.floorInBST(root , 24);
+    cout<<"Floor value: "<<ans<<endl;
+    return 0;
+}
