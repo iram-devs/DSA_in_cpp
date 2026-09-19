26863930a81275b9a71a17a8708de941cada4211
iram-devs <iramshamim2@gmail.com>
2026-09-19 14:39:46 +0530

    added inorder predecessor

---
+++ BinarySearchTree/Medium/InorderPredecessorInBST/InorderPredecessorInBST.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(): val(0),left(nullptr),right(nullptr){}
+    TreeNode(int x):val(x),left(nullptr),right(nullptr){}
+    TreeNode(int x , TreeNode* left , TreeNode* right):val(x),left(left),right(right){}
+};
+class Solution{
+    public:
+    TreeNode* inoderSuccessor(TreeNode* root , TreeNode* p)
+    {
+        TreeNode* successor = NULL;
+        while(root!=NULL)
+        {
+            if(p->val>=root->val)
+            {
+                root=root->right;
+            }
+            else
+            {
+                successor=root;
+                root=root->left;
+            }
+        }
+        return successor;
+    }
+};
+int main()
+{
+    TreeNode* root = new TreeNode(6);
+    root->left = new TreeNode(4);
+    root->right = new TreeNode(8);
+    root->left->left = new TreeNode(2);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(7);
+    root->right->right = new TreeNode(9);
+
+    Solution obj;
+    TreeNode* ans = obj.inoderSuccessor(root , root->left->right);
+    cout<<ans->val<<endl;
+    return 0;
+
+}
